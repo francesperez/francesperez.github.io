e0efe880ea358b25f20bc483a09a02c19c56e803
francesperez <frances.perez141@gmail.com>
2023-01-11 20:54:15 -0600

    autosave command

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -3,13 +3,13 @@
 let rotateMe = {x:10,y:12,z:16};
 
 ScrollReveal().reveal('.reveal', {
-    delay: 600,
+    delay: 400,
     duration: 1600,
 });
 
 ScrollReveal().reveal('.card1', {
     origin: 'left',
-    delay: 800,
+    delay: 600,
     duration: 1600,
     distance: '1000px',
     rotate: rotateMe,
@@ -17,7 +17,7 @@ ScrollReveal().reveal('.card1', {
 
 ScrollReveal().reveal('.card2', {
     origin: 'left',
-    delay: 700,
+    delay: 500,
     duration: 1600,
     distance: '1000px',
     rotate: rotateMe,
@@ -25,7 +25,7 @@ ScrollReveal().reveal('.card2', {
 
 ScrollReveal().reveal('.card3', {
     origin: 'left',
-    delay: 600,
+    delay: 400,
     duration: 1600,
     distance: '1000px',
     rotate: rotateMe,
@@ -33,7 +33,7 @@ ScrollReveal().reveal('.card3', {
 
 ScrollReveal().reveal('.headshot', {
     origin: 'left',
-    delay: 800,
+    delay: 600,
     duration: 1600,
     distance: '1000px',
 })
@@ -41,14 +41,14 @@ ScrollReveal().reveal('.headshot', {
 
 ScrollReveal().reveal('.bio', {
     origin: 'right',
-    delay: 800,
+    delay: 600,
     duration: 1600,
     distance: '1000px',
 })
 
 ScrollReveal().reveal('.scroll', {
     origin: 'right',
-    delay: 800,
+    delay: 600,
     duration: 1600,
     distance: '1000px',
 })
